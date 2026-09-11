b9a78216e06fbae12e82d3efc9aa466c209d8b08
Felber Colin <colin.felber@stud.hslu.ch>
2026-09-11 11:48:11 +0200

    Add MongoDB stack; fetch users from API
     service constructor.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,4 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import {routes} from './config/routes.config';
 
@@ -6,6 +7,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
     provideRouter(routes),
   ]
 };
--- frontend/src/app/features/user/services/user.service.ts
+++ frontend/src/app/features/user/services/user.service.ts
@@ -1,18 +1,23 @@
-import { Injectable } from '@angular/core';
-import { DUMMY_USERS } from '../user';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject, signal } from '@angular/core';
 import { User } from '../user.types';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private readonly _users = [...DUMMY_USERS];
+  private readonly http = inject(HttpClient);
+  private readonly _users = signal<User[]>([]);
 
   get users(): User[] {
-    return [...this._users];
+    return this._users();
   }
 
   findUserById(id: string): User | undefined {
-    return this._users.find(user => user.id === id);
+    return this._users().find(user => user.id === id);
+  }
+
+  constructor() {
+    this.http.get<User[]>('/api/users').subscribe(users => this._users.set(users));
   }
 }
